07a0bd8e36b41c2543c082468e6d3aef7a2dbfcf
Djdaan Truong <daantd.a12017@gmail.com>
2022-07-02 03:38:51 +0700

    update interior

--- interior-consultant/index.js
+++ interior-consultant/index.js
@@ -1,3 +1,15 @@
-function toggleMobileMenu(menu) {
-  menu.classList.toggle("open");
-}
+const menu = document.querySelector('.menu-wrapper')
+const menuOpen = document.querySelector('.menuOpen')
+const menuClose = document.querySelector('.menuClose')
+
+menuOpen.addEventListener('click', () => {
+  menu.classList.add('active')
+})
+
+menuClose.addEventListener('click', () => {
+  menu.classList.remove('active')
+})
+
+// function toggleMobileMenu(menu) {
+//   menu.classList.toggle('open')
+// }
